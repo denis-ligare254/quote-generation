88d4a9ca675c210de519a76a3c4a0c87ab5c71d8
denis <mrisholigare@gmail.com>
2020-11-15 16:46:53 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { VotesComponent } from './votes/votes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    DateCountPipe,
+    QuoteFormComponent,
+    VotesComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote} from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes:Quote[] = [
+    
+  ];
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  addNewQuote(quote){
+   let quoteLength = this.quotes.length;
+   quote.id = quoteLength+1;
+   quote.completeDate = new Date(quote.completeDate)
+   this.quotes.push(quote)
+ }
+
+ deleteQuote(isDelete, index){
+   if (isDelete) {
+     let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+ 
+     if (toDelete){
+       this.quotes.splice(index,1)
+     }
+   }
+ }
+   constructor() { }
+ 
+   ngOnInit() {
+   }
+ 
+ }
